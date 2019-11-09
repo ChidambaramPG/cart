fd3284dd3f0937926a16db6025cc062401c9d73a
ChidambaramPG <chidambarampg@gmail.com>
2019-11-09 21:30:54 +0530

    deploy

---
+++ precache-manifest.5b83fabf0f31a0d8b84b0cc5efc4d37a.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f35f9715df4e39e55b7",
+    "url": "/cart/css/app.4aa90e3a.css"
+  },
+  {
+    "revision": "30103d82a16fcd7e1d6f12d8848c4b66",
+    "url": "/cart/img/beef-steak.30103d82.jpeg"
+  },
+  {
+    "revision": "ca4f5c21b94ba2931185ef40117edfc0",
+    "url": "/cart/img/beef-steak.ca4f5c21.jpg"
+  },
+  {
+    "revision": "3bf7e171e13a4bca3e38550193600b51",
+    "url": "/cart/img/buger-nd-fries.3bf7e171.jpeg"
+  },
+  {
+    "revision": "43f33fed8fb6c525d8c137ea96d8cc9b",
+    "url": "/cart/img/buger-nd-fries.43f33fed.jpg"
+  },
+  {
+    "revision": "37a74563ad0fdfdb5b93160c80163878",
+    "url": "/cart/img/burger.37a74563.jpg"
+  },
+  {
+    "revision": "8c7524c3d91f825b14f75ba89418f50d",
+    "url": "/cart/img/burger.8c7524c3.jpeg"
+  },
+  {
+    "revision": "307f89c85bfe4947d927dd9face120af",
+    "url": "/cart/img/cheese-fries.307f89c8.jpeg"
+  },
+  {
+    "revision": "38891f6838a52f9e59787024fcf41434",
+    "url": "/cart/img/cheese-fries.38891f68.jpg"
+  },
+  {
+    "revision": "2eae733a46dbad1068729f85a4b2ea90",
+    "url": "/cart/img/chicken-biriyani.2eae733a.jpeg"
+  },
+  {
+    "revision": "4c65e1856a52c4fc93427368a5a665eb",
+    "url": "/cart/img/chicken-biriyani.4c65e185.jpg"
+  },
+  {
+    "revision": "c8a37aad437f33d9db749e78682ecaa2",
+    "url": "/cart/img/header-bg.c8a37aad.jpg"
+  },
+  {
+    "revision": "54e378e0d2bfcdfbc0711111f7c0d5e9",
+    "url": "/cart/img/lasagnia.54e378e0.jpg"
+  },
+  {
+    "revision": "8ce61e68b0de18bd4e1c9f6d211249be",
+    "url": "/cart/img/lasagnia.8ce61e68.jpeg"
+  },
+  {
+    "revision": "1d11f4fc94a9f0aa03b14fff7d25e6de",
+    "url": "/cart/img/masala-dosa.1d11f4fc.jpg"
+  },
+  {
+    "revision": "2dd97c0dcc98fccddc6aeee41eaece22",
+    "url": "/cart/img/masala-dosa.2dd97c0d.jpeg"
+  },
+  {
+    "revision": "50c02f906d0156c237abdc5bc9f8c080",
+    "url": "/cart/img/milk-and-serials.50c02f90.jpeg"
+  },
+  {
+    "revision": "9c859b485447c51022f07cbb0666ada8",
+    "url": "/cart/img/milk-and-serials.9c859b48.jpg"
+  },
+  {
+    "revision": "46bcc836db162c1ebfaaca4535e39b55",
+    "url": "/cart/img/pancake.46bcc836.jpg"
+  },
+  {
+    "revision": "7d62c47229213317fd81d2b1712505e5",
+    "url": "/cart/img/pancake.7d62c472.jpeg"
+  },
+  {
+    "revision": "004b67e24fe0644d5b5514bbc946f5de",
+    "url": "/cart/img/salad.004b67e2.jpg"
+  },
+  {
+    "revision": "669da741addaa70c5dcb9b2e136ba5cc",
+    "url": "/cart/img/salad.669da741.jpeg"
+  },
+  {
+    "revision": "4a3e169547575ece2970b4ad34c1595f",
+    "url": "/cart/img/salamon-steak.4a3e1695.jpg"
+  },
+  {
+    "revision": "fa5c6150967a44381b71b803001df0b2",
+    "url": "/cart/img/salamon-steak.fa5c6150.jpeg"
+  },
+  {
+    "revision": "6578162ddaf75b40119cba28ea876eb3",
+    "url": "/cart/img/spicy-roll.6578162d.jpeg"
+  },
+  {
+    "revision": "b03cc6d59d34f45766829bbc5f03126a",
+    "url": "/cart/img/spicy-roll.b03cc6d5.jpg"
+  },
+  {
+    "revision": "b61d7bd1f5be496a7b34309f87d1aa2c",
+    "url": "/cart/index.html"
+  },
+  {
+    "revision": "8f35f9715df4e39e55b7",
+    "url": "/cart/js/app.66197cf6.js"
+  },
+  {
+    "revision": "66b43f9a16e7a6324c83",
+    "url": "/cart/js/chunk-vendors.53dc08f6.js"
+  },
+  {
+    "revision": "4dddc0d3f83677b880ce50d8224c64d0",
+    "url": "/cart/manifest.json"
+  },
+  {
+    "revision": "5d6826e4f2c0f075ffd190a6fa4cfee3",
+    "url": "/cart/products.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/cart/robots.txt"
+  }
+]);
